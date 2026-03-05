99e8427581578f11318a94212bccdd34f9140a0c
RaviJain2803 <jravi1101@gmail.com>
2026-03-06 00:24:39 +0530

    update admin

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props)=>{
 
     const [cartItems , setCartItems] = useState({});
-    const url = "https://food-delivery-app-tg69.onrender.com";
+    const url = "https://food-delivery-app-backend-gqm6.onrender.com"
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([]);
 
