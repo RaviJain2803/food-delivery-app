c01b15aa4ecc86f31e3111c2e0bdfc17b8085dda
RaviJain2803 <jravi1101@gmail.com>
2026-06-05 17:55:28 +0530

    fix mongodb connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 export const connectDB = async()=>{
-   await mongoose.connect('mongodb+srv://jravi1101:28031999@cluster0.4rtdsqh.mongodb.net/food-delivery').then(()=>{
+   await mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("DB Connected")
     
+   }).catch((error)=>{
+      console.log(error)
    })
 }
